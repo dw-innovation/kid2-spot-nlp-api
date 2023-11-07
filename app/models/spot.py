0921bd2d7ae44127424954526e310585cbfed943
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2023-11-07 17:41:10 +0100

    add custom json parser

--- app/models/spot.py
+++ app/models/spot.py
@@ -1,29 +1,45 @@
 import torch
+import re
 import requests
 from diskcache import Cache
 import json
 import dirtyjson
 import os
 from loguru import logger
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, Text2TextGenerationPipeline
 from peft import PeftModel
+from dotenv import load_dotenv
 
 logger.add(f"{__name__}.log", rotation="500 MB")
 
+load_dotenv()
+
 peft_model_id = os.getenv("PEFT_MODEL_ID")
 base_model = os.getenv("BASE_MODEL")
+MAX_LENGTH = int(os.getenv("MAX_LENGTH"))
+CUDA_DEVICE = os.getenv("CUDA_DEVICE")
 
 ## load base LLM model and tokenizer
 transformer_model = AutoModelForSeq2SeqLM.from_pretrained(base_model)
 tokenizer = AutoTokenizer.from_pretrained(peft_model_id)
-device = torch.device("cpu")  # Set the device to CPU
+
+if CUDA_DEVICE:
+    device = torch.device(f"cuda:{cuda_device}" if torch.cuda.is_available() else "cpu")
+else:
+    device = torch.device("cpu")
 
 # Initialize disk cache and get search endpoint
 cache = Cache("tmp")
 SEARCH_ENDPOINT = os.getenv("SEARCH_ENDPOINT")
 
 transformer_model = PeftModel.from_pretrained(transformer_model, peft_model_id)
-transformer_model.to(device)
+transformer_model = transformer_model.merge_and_unload()
+transformer_model.eval()
+
+pipeline = Text2TextGenerationPipeline(model=transformer_model, batch_size=16,
+                                       tokenizer=tokenizer,
+                                       device=device,  # model.device,
+                                       clean_up_tokenization_spaces=True)
 
 
 # Cache and fetch OpenStreetMap tags
@@ -34,12 +50,6 @@ def search_osm_tag(entity):
         url=SEARCH_ENDPOINT, params=PARAMS, verify=False
     )  # set verify to False to ignore SSL certificate
     return r.json()
-
-
-# Prepare the input sentence for the model
-def prepare_input(sentence: str):
-    input_ids = tokenizer(sentence, max_length=1024, return_tensors="pt").input_ids
-    return input_ids
 
 
 # Process the 'area' part of the result
@@ -104,36 +114,21 @@ def process_nodes(nodes: list) -> list:
     return nodes
 
 
-# Clean up the result string for JSON conversion
-def clean_result(result: str) -> str:
-    result = result.strip().strip("{").strip("}")
-    result = (
-        result.replace("'", '"')
-        .replace("'", '"')
-        .replace('"{ ', "'{ ")
-        .replace('} "', "} '")
-        .replace('""', '"')
-        .replace(', "', ', "')
-        .replace("} ", "}")
-        .replace("} ]", "}]")
-        .replace(": [", ":[")
-        .replace("}}]", "}]")
-    )
-
-    return result
-
-
 # Main inference function
 def inference(sentence: str) -> str:
     # Prepare input and perform inference
-    input_data = prepare_input(sentence=sentence)
-    input_data = input_data.to(transformer_model.device)
-    outputs = transformer_model.generate(inputs=input_data, max_length=1024)
-    raw_result = tokenizer.decode(token_ids=outputs[0], skip_special_tokens=True)
+    sentence = sentence.lower()
+    result = pipeline([sentence], do_sample=False, max_length=MAX_LENGTH, pad_token_id=tokenizer.pad_token_id)
+    json_str = result[0]["generated_text"]
 
     # Post-process the results
-    result = clean_result(raw_result)
-    result = json.loads(dirtyjson.loads(result))
+    try:
+        result = json.loads(dirtyjson.loads(json_str))
+    except json.decoder.JSONDecodeError as e:
+        result = fix_json(json_str)
+
+    print("result")
+    print(result)
 
     # Process and delete "a" if present
     if "a" in result:
@@ -151,6 +146,61 @@ def inference(sentence: str) -> str:
         del result["es"]
 
     return {"result": result, "raw": raw_result}
+
+
+# Clean up the result string for JSON conversion
+def fix_json(text, decoder=json.JSONDecoder()):
+    fixed_data = {}
+    pos = 0
+    while True:
+        match = text.find('{', pos)
+
+        if match == -1:
+            break
+        try:
+            sub_text = text[match:]
+            if "\"a\":" in sub_text:
+                print("subtext")
+                print(sub_text)
+                start_index = sub_text.find('\"a\":')
+                end_index = sub_text.find('\"es\":')
+                input_text = sub_text[start_index + len('\"a\":'):end_index]
+                area_obj = dirtyjson.loads(input_text)
+                if area_obj["t"] != "bbox":
+                    fixed_data["a"] = {"t": area_obj["t"], "v": area_obj["v"]}
+                else:
+                    fixed_data["a"] = {"t": area_obj["t"]}
+            if "\"es\"" in sub_text:
+                start_index = sub_text.find('\"es\":')
+                end_index = sub_text.find('\"ns\":')
+                input_text = sub_text[start_index + len('\"es\":'):end_index]
+                es_objs = dirtyjson.loads(input_text)
+
+                fixed_data["es"] = []
+                for es_obj in es_objs:
+                    fixed_data["es"].append({"src": es_obj["src"],
+                                             "tgt": es_obj["tgt"],
+                                             "t": es_obj["t"],
+                                             "dist": es_obj["dist"]
+                                             })
+            if "\"ns\"" in sub_text:
+                start_index = sub_text.find('\"ns\":')
+                end_index = sub_text.rfind('}')
+                input_text = sub_text[start_index + len('\"ns\":'):end_index]
+                input_text = input_text.replace("}} ]", "}]")
+                ns_objs = dirtyjson.loads(input_text)
+                fixed_data["ns"] = []
+                for ns_obj in ns_objs:
+                    fixed_data["ns"].append({"id": ns_obj["id"],
+                                             "n": ns_obj["n"]
+                                             })
+            result, index = decoder.raw_decode(text[match:])
+            pos = match + index
+        except ValueError:
+            print("error")
+            pos = match + 1
+
+    return fixed_data
 
 
 def replace_keys_recursive(filters):
